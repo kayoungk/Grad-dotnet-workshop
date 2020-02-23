5916e83daa8a01818c2300017e0906da40934a50
William Tulloch <william.tulloch@readify.net>
2020-02-23 22:36:28 +1100

    Start project

--- WorkshopLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WorkshopLibrary
-{
-    public class Class1
-    {
-    }
-}
---
+++ WorkshopLibrary/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkshopLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
